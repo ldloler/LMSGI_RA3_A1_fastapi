4fc99f1ee18d0b97f8c1dbf4651fa2dcf7e6e056
ldloler <marc.be.pi@gmail.com>
2025-03-18 16:30:47 +0100

    Part 1 finalitzada

--- gestor.py
+++ gestor.py
@@ -26,10 +26,57 @@ def llegir():
     fitxer.close()
     return dic
 
-# Desa el parametre dic al fitxer, transformantse en json.
-def desar(dic: dict):
+# Desa el parametre dic al fitxer, transformant-lo en json.
+def desar(dic: list[dict]):
     fitxer = open("alumnes.json", "wt")
-    s = json.dumps(dic)
+    s = json.dumps(dic, indent=4)
     fitxer.write(s)
     fitxer.close()
     return "Desat correctament"
+
+# Mostra l'id, el nom i el cognom de tots els alumnes.
+def mostra_tots():
+    alumnes = llegir()
+    for alumne in alumnes:
+        print(alumne["id"], alumne["nom"], alumne["cognom"])
+
+# Afegeix un alumne.
+def afegir_alumne(nom: str, cognom: str, dia: int, mes: int, any: int, email: str, feina: bool, curs: str):
+    alumnes = llegir()
+    id = alumnes[len(alumnes) - 1]["id"] + 1
+    alumne = {
+        "id": id,
+        "nom": nom,
+        "cognom": cognom,
+        "data": {
+            "dia": dia,
+            "mes": mes,
+            "any": any
+        },
+        "email": email,
+        "feina": feina,
+        "curs": curs
+    }
+    alumnes.append(alumne)
+    desar(alumnes)
+    return "Alumne afegit"
+
+# Mostra tots els detalls d'un alumne segons l'id.
+def veure_alumne(id: int):
+    alumnes = llegir()
+    for alumne in alumnes:
+        if alumne["id"] == id:
+            result = json.dumps(alumne, indent=4)
+            return result
+    return "L'alumne amb id: {} no existeix".format(id)
+
+# Esborra un alumne segons l'id
+def esborra(id: int):
+    alumnes = llegir()
+    for alumne in alumnes:
+        if alumne["id"] == id:
+            i = alumnes.index(alumne)
+            alumnes.pop(i)
+            desar(alumnes)
+            return "Alumne esborrat"
+    return "L'alumne amb id: {} no existeix".format(id)
