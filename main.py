3fea893346e156ae2fad834a577a9454a7ebd9ee
ldloler <marc.be.pi@gmail.com>
2025-04-01 10:22:35 +0200

    Finalitzat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import gestor as g
+from gestor import llegir
 
 # Creacció d'app interactiva FastAPI
 app = FastAPI()
@@ -19,14 +20,17 @@ def total_alumnes():
 # Retorna les dades de l’alumne amb id igual a número.
 @app.get("/id/{numero}/")
 def read_empleat(numero: int):
-    return g.veure_alumne(numero)
+    alumnes = g.llegir()
+    return g.veure_alumne(alumnes, numero)
 
 # Esborra l’alumne amb id igual a número.
 @app.delete("/del/{numero}/")
 def esborra_empleat(numero: int):
-    return g.esborra(numero)
+    alumnes = g.llegir()
+    return g.esborra(alumnes, numero)
 
 # Afegir un alumne amb les dades proporcionades.
 @app.post("/alumne/")
 def crear_empleat(nom: str, cognom: str, dia: int, mes: int, any: int, email: str, feina: bool, curs: str):
-    return g.afegir_alumne(nom, cognom, dia, mes, any, email, feina, curs)
+    alumnes = g.llegir()
+    return g.afegir_alumne(alumnes, nom, cognom, dia, mes, any, email, feina, curs)
